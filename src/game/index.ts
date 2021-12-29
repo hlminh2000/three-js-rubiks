4d3a5a08f2367f5884b893d4c97adbf71e3363a6
Minh <hlminh2000@gmail.com>
2021-12-29 08:13:01 -0500

    💩 tests out vr without bloom

--- src/game/index.ts
+++ src/game/index.ts
@@ -28,19 +28,19 @@ export function init({ domContainer }: { domContainer: HTMLDivElement }) {
   renderer.setAnimationLoop(animation);
   domContainer.appendChild(renderer.domElement);
 
-  const bloomPass = new UnrealBloomPass(
-    new THREE.Vector2(window.innerWidth, window.innerHeight),
-    1.5,
-    0.4,
-    0.85
-  );
-  bloomPass.threshold = 0;
-  bloomPass.strength = 0.75;
-  bloomPass.radius = 0.5;
-  const renderScene = new RenderPass(scene, camera);
-  const composer = new EffectComposer(renderer);
-  composer.addPass(renderScene);
-  composer.addPass(bloomPass);
+  //   const bloomPass = new UnrealBloomPass(
+  //     new THREE.Vector2(window.innerWidth, window.innerHeight),
+  //     1.5,
+  //     0.4,
+  //     0.85
+  //   );
+  //   bloomPass.threshold = 0;
+  //   bloomPass.strength = 0.75;
+  //   bloomPass.radius = 0.5;
+  //   const renderScene = new RenderPass(scene, camera);
+  //   const composer = new EffectComposer(renderer);
+  //   composer.addPass(renderScene);
+  //   composer.addPass(bloomPass);
 
   const gridHelper = new THREE.GridHelper(10, 100);
   scene.add(gridHelper);
@@ -51,7 +51,8 @@ export function init({ domContainer }: { domContainer: HTMLDivElement }) {
     rubiksCube.rotation.x = time / 2000;
     rubiksCube.rotation.y = time / 1000;
     controls.update();
-    composer.render();
+    // composer.render();
+    renderer.render(scene, camera);
   }
 
   const vrButton = VRButton.createButton(renderer);
